972662111c460c1effe29800c068ae9f5add69b4
Jlphile <lkyz001@outlook.com>
2022-09-22 07:22:02 +0800

    25-master

---
+++ src/App_nexttick.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2 class="title">{{ message }}</h2>
+    <button @click="addMessageContent">添加内容</button>
+  </div>
+</template>
+
+<script>
+import { ref, onUpdated } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('');
+    const titleRef = ref(null);
+    const addMessageContent = () => {
+      message.value += '哈哈哈哈哈';
+    };
+    onUpdated(() => {
+      console.log(titleRef);
+    });
+    return {
+      message,
+      addMessageContent,
+      titleRef,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  width: 120px;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,16 +9,26 @@
 </template>
 
 <script>
-import { mapState, mapGetters, mapMutations, mapActions } from 'vuex';
+// import {
+//   mapState,
+//   mapGetters,
+//   mapMutations,
+//   mapActions,
+//   createNamespacedHelpers,
+// } from 'vuex';
+import { createNamespacedHelpers } from 'vuex';
+import { useState, useGetters } from '../hooks/index';
+const { mapActions, mapState, mapMutations, mapGetters } =
+  createNamespacedHelpers('home');
 export default {
   computed: {
     // 1.写法一
-    ...mapState({
-      homeCounter: (state) => state.home.homeCounter,
-    }),
-    ...mapGetters({
-      doubleHomeCounter: 'home/doubleHomeCounter',
-    }),
+    // ...mapState({
+    //   homeCounter: (state) => state.home.homeCounter,
+    // }),
+    // ...mapGetters({
+    //   doubleHomeCounter: 'home/doubleHomeCounter',
+    // }),
     // 2.写法二
     // ...mapState('home', ['homeCounter']),
     // ...mapGetters('home', ['doubleHomeCounter']),
@@ -27,12 +37,34 @@ export default {
     // ...mapGetters(['doubleHomeCounter']),
   },
   methods: {
-    ...mapMutations({
-      increment: 'home/increment',
-    }),
-    ...mapActions({
-      incrementAction: 'home/incrementAction',
-    }),
+    // 1.写法一
+    // ...mapMutations({
+    //   increment: 'home/increment',
+    // }),
+    // ...mapActions({
+    //   incrementAction: 'home/incrementAction',
+    // }),
+    // 2.写法二
+    // ...mapMutations('home', ['increment']),
+    // ...mapActions('home', ['incrementAction']),
+    // 3.写法三
+    // ...mapMutations(['increment']),
+    // ...mapActions(['incrementAction']),
+  },
+  setup() {
+    // {homeCounter:function}
+    const state = useState('home', ['homeCounter']);
+    const getters = useGetters('home', ['doubleHomeCounter']);
+
+    const mutations = mapMutations(['increment']);
+    const actions = mapActions(['incrementAction']);
+
+    return {
+      ...state,
+      ...getters,
+      ...mutations,
+      ...actions,
+    };
   },
 };
 </script>
