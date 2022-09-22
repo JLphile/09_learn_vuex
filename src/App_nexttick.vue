eabdb750cdd58cf76249f9289dcb29e7e4cd8381
Jlphile <lkyz001@outlook.com>
2022-09-22 15:01:39 +0800

    25-nextTick

--- src/App_nexttick.vue
+++ src/App_nexttick.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <h2 class="title">{{ message }}</h2>
+    <h2>{{ counter }}</h2>
+    <button @click="increment">+1</button>
+    <h2 class="title" ref="titleRef">{{ message }}</h2>
     <button @click="addMessageContent">添加内容</button>
   </div>
 </template>
 
 <script>
-import { ref, onUpdated } from 'vue';
+import { ref, onUpdated, nextTick } from 'vue';
 
 export default {
   setup() {
+    const counter = ref(0);
     const message = ref('');
     const titleRef = ref(null);
     const addMessageContent = () => {
       message.value += '哈哈哈哈哈';
+      // 更新DOM后再执行nextTick
+      nextTick(() => console.log(titleRef.value.offsetHeight));
     };
-    onUpdated(() => {
-      console.log(titleRef);
-    });
+    const increment = () => counter.value++;
+    // onUpdated(() => {
+    //   console.log(titleRef.value.offsetHeight);
+    // });
     return {
       message,
       addMessageContent,
       titleRef,
+      counter,
+      increment,
     };
   },
 };
